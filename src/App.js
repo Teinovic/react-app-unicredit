fa4fb788b749758cd0f452518e9a6556e64c2832
Teinovic <momir.teinovic.2011@gmail.com>
2021-09-26 01:00:48 +0200

    started working on the form

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react'
 import { store } from './actions/store'
 import { Provider } from 'react-redux'
 import BookInstances from './components/BookInstances'
+import BookInstanceForm from './components/BookInstanceForm'
 
 function App() {
   return (
     <Provider store={store}>
       <BookInstances />
+      <BookInstanceForm />
     </Provider>
   )
 }
